2f16762d27a463da9e3031ba175b1a1db9795dd7
Saurabh Bhandari <f20212412@pilani.bits-pilani.ac.in>
2024-02-16 21:50:30 +0530

    Completed DFA encoding template, Finished Start state transitions.
    Co-authored-by: Abir Abhyankar <Abir2004@users.noreply.github.com>
    Co-authored-by: Lakshit-Sethi <Lakshit-Sethi@users.noreply.github.com>

---
+++ lexer.c
@@ -0,0 +1,247 @@
+#include "lexerDef.h"
+
+void add_state(state_id state, transition *transitions, int transition_count)
+{
+    // TODO: Implement the case when the state already exists
+    states[state].transitions = transitions;
+}
+
+transition f(char ch, state_id next_state)
+{
+    transition t = (transition)malloc(sizeof(Transition));
+    t->next_state = &states[next_state];
+    t->next_char = ch;
+    return t;
+}
+
+transition *theta(char except_char, state_id next_state)
+{
+    int size = sizeof(alphabets) / sizeof(char) - 1;
+    transition *transitions = (transition *)malloc(size * sizeof(transition));
+    int j = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (alphabets[i] != except_char)
+        {
+            transitions[j++] = f(alphabets[i], next_state);
+        }
+    }
+    return transitions;
+}
+
+transition *aToZExceptBToD(state_id next_state)
+{
+    int size = 23;
+    transition *transitions = (transition *)malloc(size * sizeof(transition));
+    int j = 0;
+    for (int i = 'a'; i <= 'z'; i++)
+    {
+        if (i != 'b' && i != 'c' && i != 'd')
+        {
+            transitions[j++] = f(i, next_state);
+        }
+    }
+    return transitions;
+}
+
+transition *aToZ(state_id next_state)
+{
+    int size = 26;
+    transition *transitions = (transition *)malloc(size * sizeof(transition));
+    int j = 0;
+    for (int i = 'a'; i <= 'z'; i++)
+    {
+        transitions[j++] = f(i, next_state);
+    }
+    return transitions;
+}
+
+transition *AToZ(state_id next_state)
+{
+    int size = 52;
+    transition *transitions = (transition *)malloc(size * sizeof(transition));
+    int j = 0;
+    for (int i = 'a'; i <= 'z'; i++)
+    {
+        transitions[j++] = f(i, next_state);
+    }
+    for (int i = 'A'; i <= 'Z'; i++)
+    {
+        transitions[j++] = f(i, next_state);
+    }
+    return transitions;
+}
+
+transition *bToD(state_id next_state)
+{
+    int size = 3;
+    transition *transitions = (transition *)malloc(size * sizeof(transition));
+    int j = 0;
+    for (int i = 'b'; i <= 'd'; i++)
+    {
+        transitions[j++] = f(i, next_state);
+    }
+    return transitions;
+}
+
+transition *zeroToNine(state_id next_state)
+{
+    int size = 10;
+    transition *transitions = (transition *)malloc(size * sizeof(transition));
+    int j = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        transitions[j++] = f(i + '0', next_state);
+    }
+    return transitions;
+}
+
+transition *twoToSeven(state_id next_state)
+{
+    int size = 6;
+    transition *transitions = (transition *)malloc(size * sizeof(transition));
+    int j = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        transitions[j++] = f(i + '2', next_state);
+    }
+    return transitions;
+}
+
+void initialize_states()
+{
+    for (int i = 0; i < MAX_STATES; i++)
+    {
+        states[i].length = 0;
+        states[i].state_id = i;
+    }
+    int theta_size = sizeof(alphabets) / sizeof(char) - 1;
+    add_state(START, (transition[]){f('%', S_0), f(' ', START), f('@', S_19), f('&', S_22), f('#', S_25), f('*', TK_MUL), f('>', S_29), f('<', S_32), f('/', TK_DIV), f('\t', START), f('\n', START), f('~', TK_NOT), f('[', TK_SQL), f(']', TK_SQR), f(',', TK_COMMA), f(';', TK_SEM), f(':', TK_COLON), f('.', TK_DOT), f('(', TK_OP), f(')', TK_CL), f('=', TK_EQ), f('+', TK_PLUS), f('-', TK_MINUS), f('!', S_17)}, 24);
+    add_state(START, bToD(S_46), 3);
+    add_state(START, zeroToNine(S_50), 10);
+    add_state(START, aToZExceptBToD(S_44), 23);
+    add_state(S_0, theta('%', S_0), theta_size);
+    add_state(S_0, (transition[]){f('%', S_1)}, 1);
+
+    // TODO: Odd starting states
+    add_state(S_19, (transition[]){f('@', S_20)}, 1);
+    add_state(S_23, (transition[]){f('&', TK_AND)}, 1);
+    add_state(S_25, aToZ(S_26), 26);
+    add_state(S_29, theta('=', TK_GT), theta_size);
+    add_state(S_29, (transition[]){f('=', TK_GE)}, 1);
+    add_state(S_35, theta('-', S_36), theta_size);
+    add_state(S_35, (transition[]){f('-', S_37)}, 1);
+    add_state(S_37, (transition[]){f('-', TK_ASSIGNOP)}, 1);
+
+    // TODO: Even starting states
+    add_state(S_20, (transition[]){f('@', TK_OR)}, 1);
+    add_state(S_22, (transition[]){f('&', S_23)}, 1);
+    add_state(S_26, theta('$', TK_RUID), theta_size + 1);
+    add_state(S_26, aToZ(S_26), 26);
+    add_state(S_32, theta('$', TK_LT), theta_size + 1);
+    add_state(S_32, (transition[]){f('=', TK_LE), f('-', S_35)}, 1);
+    add_state(S_40, AToZ(41), 52);
+    add_state(S_42, zeroToNine(S_42), 10);
+    add_state(S_42, theta('$', S_43), theta_size + 1);
+    add_state(S_44, aToZ(S_44), 26);
+    add_state(S_44, theta('$', S_45), theta_size + 1);
+    add_state(S_46, twoToSeven(S_47), 26);
+    add_state(S_46, aToZ(S_44), 26);
+    add_state(S_48, twoToSeven(S_48), 6);
+    add_state(S_48, theta('$', TK_ID), theta_size + 1);
+    add_state(S_50, zeroToNine(S_50), 10);
+}
+
+FILE *getStream(FILE *fp)
+{
+    if (fp == NULL)
+    {
+        printf("Error: File not found\n");
+        exit(1);
+    }
+    twinBuffer buffer = (twinBuffer)malloc(sizeof(struct TwinBuffer));
+    buffer->secondary_buffer_index = 0;
+    // make a new .salad file
+    // get the file name of fp
+    FILE *new_fp = fopen("a.salad", "w");
+    if (new_fp == NULL)
+    {
+        printf("Error: Could not create a new file\n");
+        exit(1);
+    }
+    while (fgets(buffer->primary_buffer, MAX_BUFFER_SIZE, fp) != NULL)
+    {
+        tokenInfo info = getNextToken(buffer);
+        for (int i = 0; i < info->token_count; i++)
+        {
+            fprintf(new_fp, "%d ", info->tokens[i]);
+        }
+    }
+    fclose(fp);
+    return new_fp;
+}
+
+tokenInfo getNextToken(twinBuffer buffer)
+{
+    state curr_state = &states[START];
+    state prev_state = NULL;
+    // read from secondary buffer first if it is not empty
+
+    tokenInfo tokens = (tokenInfo)malloc(sizeof(TokenInfo));
+    tokens->tokens = (int *)malloc(MAX_BUFFER_SIZE * sizeof(int));
+    tokens->token_count = 0;
+
+    while (buffer->secondary_buffer_index > 0)
+    {
+        curr_state = get_next_state(curr_state, buffer->secondary_buffer[buffer->secondary_buffer_index]);
+        buffer->secondary_buffer_index--;
+    }
+
+    // read from primary buffer
+    int token_len = 0;
+    int token_count = 0;
+    while (buffer->primary_buffer_index < MAX_BUFFER_SIZE)
+    {
+        curr_state = get_next_state(curr_state, buffer->primary_buffer[buffer->primary_buffer_index]);
+        if (buffer->primary_buffer[buffer->primary_buffer_index] == '\n')
+        {
+            line_count++;
+        }
+        if (curr_state == &states[START] && prev_state != NULL)
+        {
+            tokens->tokens[tokens->token_count] = prev_state->state_id;
+            tokens->token_count++;
+        }
+        token_len++;
+        buffer->primary_buffer_index++;
+        prev_state = curr_state;
+    }
+    if (curr_state != &states[START])
+    {
+        strncpy(buffer->secondary_buffer, buffer->primary_buffer + MAX_BUFFER_SIZE - 1 - token_len, token_len);
+        buffer->secondary_buffer_index = token_len;
+    }
+    else
+    {
+        buffer->secondary_buffer_index = 0;
+    }
+
+    return tokens;
+}
+
+state get_next_state(state current_state, char next_char)
+{
+    for (int i = 0; i < current_state->length; i++)
+    {
+        if (current_state->transitions[i]->next_char == next_char)
+        {
+            return current_state->transitions[i]->next_state;
+        }
+    }
+    return &states[TK_INVALID];
+}
+
+int main()
+{
+    initialize_states();
+}
---
+++ lexerDef.h
@@ -0,0 +1,187 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_BUFFER_SIZE 50
+#define MAX_STATES 150
+
+const char alphabets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                          'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                          'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                          'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                          'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2',
+                          '3', '4', '5', '6', '7', '8', '9', ' ', '\n', '\t', '~',
+                          '(', '[', ']', ')', '<', '>', '!', '@', '#', '%', '&',
+                          '*', '_', '+', '-', '/', '=', ';', ':', '.', ','};
+int line_count = 0;
+
+typedef struct Transition
+{
+    char next_char;
+    struct State *next_state;
+} Transition;
+
+typedef struct State
+{
+    int state_id;
+    struct Transition **transitions;
+    int length;
+} State;
+
+typedef struct TwinBuffer
+{
+    char primary_buffer[MAX_BUFFER_SIZE];
+    char secondary_buffer[MAX_BUFFER_SIZE];
+    int primary_buffer_index;
+    int secondary_buffer_index;
+} TwinBuffer;
+
+typedef struct TokenInfo
+{
+    int *tokens;
+    int token_count;
+} TokenInfo;
+
+typedef struct State *state;
+typedef struct Transition *transition;
+typedef struct TwinBuffer *twinBuffer;
+typedef struct TokenInfo *tokenInfo;
+
+State states[MAX_STATES];
+
+typedef enum state_id
+{
+    START,
+    TK_INVALID,
+    TK_ASSIGNOP,
+    TK_COMMENT,
+    TK_FIELDID,
+    TK_ID,
+    TK_NUM,
+    TK_RNUM,
+    TK_FUNID,
+    TK_RUID,
+    TK_WITH,
+    TK_PARAMETERS,
+    TK_END,
+    TK_WHILE,
+    TK_UNION,
+    TK_ENDUNION,
+    TK_DEFINETYPE,
+    TK_AS,
+    TK_TYPE,
+    TK_MAIN,
+    TK_GLOBAL,
+    TK_PARAMETER,
+    TK_LIST,
+    TK_SQL,
+    TK_SQR,
+    TK_INPUT,
+    TK_OUTPUT,
+    TK_INT,
+    TK_REAL,
+    TK_COMMA,
+    TK_SEM,
+    TK_COLON,
+    TK_DOT,
+    TK_ENDWHILE,
+    TK_OP,
+    TK_CL,
+    TK_IF,
+    TK_THEN,
+    TK_ENDIF,
+    TK_READ,
+    TK_WRITE,
+    TK_RETURN,
+    TK_PLUS,
+    TK_MINUS,
+    TK_MUL,
+    TK_DIV,
+    TK_CALL,
+    TK_RECORD,
+    TK_ENDRECORD,
+    TK_ELSE,
+    TK_AND,
+    TK_OR,
+    TK_NOT,
+    TK_LT,
+    TK_LE,
+    TK_EQ,
+    TK_GT,
+    TK_GE,
+    TK_NE,
+    S_0,
+    S_1,
+    S_2,
+    S_3,
+    S_4,
+    S_5,
+    S_6,
+    S_7,
+    S_8,
+    S_9,
+    S_10,
+    S_11,
+    S_12,
+    S_13,
+    S_14,
+    S_15,
+    S_16,
+    S_17,
+    S_18,
+    S_19,
+    S_20,
+    S_21,
+    S_22,
+    S_23,
+    S_24,
+    S_25,
+    S_26,
+    S_27,
+    S_28,
+    S_29,
+    S_30,
+    S_31,
+    S_32,
+    S_33,
+    S_34,
+    S_35,
+    S_36,
+    S_37,
+    S_38,
+    S_39,
+    S_40,
+    S_41,
+    S_42,
+    S_43,
+    S_44,
+    S_45,
+    S_46,
+    S_47,
+    S_48,
+    S_49,
+    S_50,
+    S_51,
+    S_52,
+    S_53,
+    S_54,
+    S_55,
+    S_56,
+    S_57,
+    S_58,
+    S_59,
+    S_60,
+    S_61
+} state_id;
+
+void add_state(state_id state_num, transition *transitions, int transition_count);
+
+void initialize_states();
+
+FILE *getStream(FILE *fp);
+
+tokenInfo getNextToken(twinBuffer buffer);
+
+state get_next_state(state current_state, char next_char);
+
+transition *theta(char except_char, state_id next_state);
